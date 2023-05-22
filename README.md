48a3a2729c1a1e8ce26b0ea77ade7c610042bd03
ILTON JUNIO CAETANO DE SOUZA <129558260+iltonjunio@users.noreply.github.com>
2023-05-22 14:56:15 -0300

    alteração

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # helloworld
 novo projeto
+EXERCÍCIO A
+Crie um novo repositório no GitHub e inicialize um projeto frontend com a estrutura de pastas e arquivos que você já conhece. Na pasta "js" você deverá criar um arquivo chamado "script.js" e importá-lo no arquivo principal index.html.
+
+EXERCÍCIO B
+Dentro do arquivo "script.js",  crie funções Javascript que executem as seguintes tarefas:
+
+
+1) Calcular a área de um círculo a partir do valor do raio informado pelo usuário.
+
+area = pi * raio²
+
+2) Calcular a hipotenusa de um triângulo-retângulo a partir dos valores dos catetos informados pelo usuário.
+
+hipotenusa² = cateto_oposto² + cateto_adjacente²
+
+3) Converter um valor em reais informado pelo usuário para dólares.
+
+Compreender a fórmula faz parte da avaliação!
+
+4) Converter uma temperatura em graus Celsius informado pelo usuário para graus Fahrenheit.
+
+fahrenheit = (celsius * 1.8) + 32
+
+5) Calcular a média aritmética entre 3 notas de um aluno informadas pelo usuário.
+
+Compreender a fórmula faz parte da avaliação!
+
+6) Obter as duas raízes de uma equação do segundo grau a partir dos valores de A, B e C informados pelo usuário (fórmula de Bhaskara).
+
+x1 = - b + raiz_quadrada(b²-4ac) / 2a
+x1 = - b - raiz_quadrada(b²-4ac) / 2a
+
+
+Entregável: cópia do arquivo script.js
--- js/script.js
+++ js/script.js
@@ -1,19 +1,68 @@
-function helloworld(){
+function helloworld() {
     alert("hello world!");
 }
 
-function soma(n1,n2) {
-    return n1 + n2;
+function soma(n1, n2) {
+    alert(n1 + n2)
+
 }
 
-function subtracao(n1,n2) {
-    return n1 + n2;
+function subtracao(n1, n2) {
+    alert(n1 + n2)
 }
 
-function multiplicacao(n1,n2) {
-    return n1 + n2;
+function multiplicacao(n1, n2) {
+    alert(n1 + n2)
 }
 
-function divisao(n1,n2) {
-    return n1 + n2;
+function divisao(n1, n2) {
+    alert(n1 + n2)
+}
+
+function mostrarnome() {
+    let nome = prompt("Qual seu nome?")
+    alert("Olá, " + nome)
+}
+
+function calculo(n1, n2) {
+    alert(n1 * n2)
+}
+
+function circulo() {
+    let raio = parseFloat(prompt("Informe o valor do raio"))
+    let area = Math.PI * Math.pow(raio, 2)
+    alert("A área do círculo é: " + area);
+}
+
+function triagulo() {
+    let cateto1 = parseFloat(prompt("Digite o valor:"))
+    let cateto2 = parseFloat(prompt("Digite o valor:"))
+    hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2))
+    alert(hipotenusa.toFixed(2))
+}
+
+
+function realdolar() {
+    let real = prompt("Digite o valor em reais:")
+    let cotacao = prompt("Digite a cotação atuação do dolar:")
+    let dolares = real / cotacao
+    alert(dolares.toFixed(2));
+}
+function media() {
+    let nota1 = parseFloat(prompt("Informe a primeira nota:"));
+    let nota2 = parseFloat(prompt("Informe a segunda nota:"));
+    let nota3 = parseFloat(prompt("Informe a terceira nota:"));
+    let media = (nota1 + nota2 + nota3) / 3;
+    alert(media.toFixed(2));
+}
+
+function raizes() {
+    let celsius = prompt(Informe a temperatura em graus: )
+    let fahrenjeit = (celsius * 1.8) + 32
+    alert(fahrenjeit)
+}
+function kmhora() {
+    let km = prompt("informe o valor em km/h")
+    let msegundo = km / 3.6
+    alert(msegundo.toFixed(2));
 }
